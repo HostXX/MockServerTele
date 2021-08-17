1e4d6264d317a0c4e1e80163a3d5f159e09d0935
carlos g <carlos.garcia@theksquaregroup.com>
2021-08-17 18:23:22 -0400

    Revert "First Commit"
    This reverts commit 586ca58a1d51792f3f3f0251aa46ea65d352fdc8.

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const mockserver = require('mockserver');
-const PORT = 9001;
- 
-mockserver.headers = ['Authorization'];
-
-
-http.createServer(mockserver('./mocks')).listen(PORT,()=>console.log('Listening on port: '+PORT));
